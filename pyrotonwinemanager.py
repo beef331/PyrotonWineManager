db2f038d6c5d80515db150c76cf001ca97dfd1c1
Jason Beetham <beefers331@gmail.com>
2019-04-25 22:24:15 -0600

    Intuitive mouse support, no more jumpy ui

--- pyrotonwinemanager.py
+++ pyrotonwinemanager.py
@@ -3,8 +3,7 @@ import os
 import urllib.request
 import json
 import subprocess
-import time
-
+import math
 
 from pyglet.window import key
 from pyglet.window import mouse
@@ -20,6 +19,7 @@ mousey = 0
 gamePaths = []
 names = []
 images = []
+currentPage = 0
 baseDir = os.environ['HOME'] + "/.config/PyrotonWineManager/"
 configPath = baseDir + ".config"
 dllPath = baseDir + ".dlls"
@@ -141,6 +141,7 @@ def on_mouse_scroll(x, y, scroll_x, scroll_y):
 			selectedOption = (selectedOption - 1 + len(dlls))%len(dlls)
 		elif(scroll_y < 0):
 			selectedOption = (selectedOption + 1 + len(dlls))%len(dlls)
+	SetCurrentPage()
 @window.event
 def on_mouse_leave(x, y):
 	global mousex
@@ -158,6 +159,7 @@ def on_key_press(symbol, modifiers):
 	global selectedGame
 	global selectedOption
 	global currentMenu
+	global currentPage
 	if(symbol == key.DOWN):
 		if(Menus(currentMenu) == Menus.GameSelect):
 			selectedGame = (selectedGame + 1 + len(gamePaths)) % len(gamePaths)
@@ -189,6 +191,7 @@ def on_key_press(symbol, modifiers):
 			elif(GameOptions(selectedOption) == GameOptions.WineTricks):
 				selectedOption = 0
 				currentMenu = Menus.Winetricks
+				currentPage = 0
 			elif(GameOptions(selectedOption) == GameOptions.Back):
 				currentMenu = 0
 			elif(GameOptions(selectedOption) == GameOptions.Terminal):
@@ -202,22 +205,38 @@ def on_key_press(symbol, modifiers):
 			currentMenu = Menus.PrefixManage
 		elif(currentMenu == Menus.PrefixManage):
 			currentMenu = Menus.GameSelect
-	
+			currentPage = math.floor(selectedGame /labelsToDraw)
+	SetCurrentPage()
+def SetCurrentPage():
+	global currentPage
+	if(Menus(currentMenu) == Menus.GameSelect):
+		totalPages = math.ceil(len(gamePaths)/labelsToDraw)
+		if(selectedGame == (currentPage * labelsToDraw + len(gamePaths)-1) % len(gamePaths)):
+				currentPage = (currentPage - 1 + totalPages) % totalPages
+		elif(selectedGame == ((currentPage + 1) * labelsToDraw + len(gamePaths)) % len(gamePaths)):
+				currentPage = (currentPage + 1 + totalPages) % totalPages
+	if(Menus(currentMenu) == Menus.Winetricks):
+		totalPages = math.ceil(len(dlls)/labelsToDraw)
+		if(selectedOption == (currentPage * labelsToDraw + len(dlls)-1) % len(dlls)):
+				currentPage = (currentPage - 1 + totalPages) % totalPages
+		elif(selectedOption == ((currentPage + 1) * labelsToDraw + len(dlls)) % len(dlls)):
+				currentPage = (currentPage + 1 + totalPages) % totalPages
 @window.event
 def on_draw():
 	global selectedGame
 	global selectedOption
 	window.clear()
 	if(Menus(currentMenu) == Menus.GameSelect):
-		offset = int(selectedGame/labelsToDraw)
-		for x in range(0,labelsToDraw ):
-			index = (int(offset * labelsToDraw + x) + len(gamePaths)) % len(gamePaths)
+		offset = currentPage * labelsToDraw
+		for x in range(0,min(len(gamePaths),labelsToDraw )):
+			index = (offset + x + len(gamePaths)) % len(gamePaths)
 			label = pyglet.text.Label(names[index],
 						font_size=20,
 						x=window.width/2, y=(window.height * .5) - (x * 30),
 						anchor_x='center', anchor_y='center')
 			if(MouseOver(label)):
 				selectedGame = index
+				SetCurrentPage()
 				GetImage()			
 			if(selectedGame == index):
 				label.color = (0,255,0,255)
@@ -236,15 +255,16 @@ def on_draw():
 				label.color = (0,255,0,255)
 			label.draw()
 	if(Menus(currentMenu) == Menus.Winetricks):
-		offset = int(selectedOption/labelsToDraw)
-		for x in range(0,labelsToDraw):
-			index = (offset * labelsToDraw + x + len(dlls))%len(dlls)
+		offset = currentPage * labelsToDraw
+		for x in range(0,min(len(dlls),labelsToDraw )):
+			index = (offset + x + len(dlls))%len(dlls)
 			label = pyglet.text.Label(dlls[index],
 						font_size=20,
 						x=window.width/2, y=(window.height * .5) - (x * 30),
 						anchor_x='center', anchor_y='center')
 			if(MouseOver(label)):
 				selectedOption = index
+				SetCurrentPage()
 			if(selectedOption == index):
 				label.color = (0,255,0,255)
 			label.draw()
